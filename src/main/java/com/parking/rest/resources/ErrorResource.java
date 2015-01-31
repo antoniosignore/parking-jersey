bb9b760e68212b0a32d0eb02b6e439533c3a7252
Mida Boghetich <mida@boghetich.com>
2015-01-31 23:29:15 +0100

    error handling
    Signed-off-by: Mida Boghetich <mida@boghetich.com>

--- src/main/java/com/parking/rest/resources/ErrorResource.java
+++ src/main/java/com/parking/rest/resources/ErrorResource.java
@@ -1,7 +1,35 @@
 package com.parking.rest.resources;
 
-/**
- * Created by mbogheti on 31/01/15.
- */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Component
+@Path("/error")
 public class ErrorResource {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Path("notfound")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response notFound() {
+        this.logger.info("Not Found!");
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @Path("unauthorized")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response unauthorized() {
+        this.logger.info("Unauthorized!");
+        return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+
+
+
 }
