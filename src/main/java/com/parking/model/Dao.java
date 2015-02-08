7a6ddf6f4de275e29a7f1c5c45c3330cbefb98b4
Antonio Signore <antonio.signore@gmail.com>
2015-02-08 15:18:13 +0100

    integrated tests in the same project

--- src/main/java/com/parking/model/Dao.java
+++ src/main/java/com/parking/model/Dao.java
@@ -4,17 +4,13 @@ import com.parking.entity.Entity;
 
 import java.util.List;
 
-
 public interface Dao<T extends Entity, I> {
 
     List<T> findAll();
 
-
     T find(I id);
 
-
     T save(T entity);
-
 
     void delete(I id);
 
